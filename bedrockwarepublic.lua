1a77f23dfe18b984568646484d7dbaecbcffe273
KidtoxicBtw <134671326+KidtoxicBtw@users.noreply.github.com>
2024-01-08 09:33:20 -0600

    changed ui lib to orion and added features

--- bedrockwarepublic.lua
+++ bedrockwarepublic.lua
@@ -9,6 +9,30 @@ local Tab = Window:MakeTab({
 local Section = Tab:AddSection({
 	Name = "Main"
 })
+local Tab2 = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section2 = Tab:AddSection({
+	Name = "Player"
+})
+local Tab3 = Window:MakeTab({
+	Name = "Utility",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section3 = Tab:AddSection({
+	Name = "Utility"
+})
+local Tab4 = Window:MakeTab({
+	Name = "Special",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Special"
+})
 local playersService = game:GetService("Players")
 local textService = game:GetService("TextService")
 local lightingService = game:GetService("Lighting")
@@ -40,3 +64,19 @@ Tab:AddButton({
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
   	end    
 })
+
+Tab2:AddSlider({
+	Name = "Speed (doesnt bypass on bedwars)",
+	Min = 0,
+	Max = 100,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "speedvalue",
+	Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speedvalue
+	end    
+})
+
+
+OrionLib:Init()
