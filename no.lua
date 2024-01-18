46f6c9d1f1833f0def876d4b2687a1c264a5403f
KidtoxicBtw <134671326+KidtoxicBtw@users.noreply.github.com>
2024-01-18 00:02:53 -0600

    Delete no.lua

--- no.lua
+++
@@ -1,9 +0,0 @@
---dont use
-local id = game.PlaceId
-local plyr = game.Players.LocalPlayer
-
-if id == 13772394625 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KidtoxicBtw/Games/main/dontuselplspls.lua")()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KidtoxicBtw/Games/main/dontuselplspls.lua")()
-end
